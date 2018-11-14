b1b65d44984f3ae7dd06cfbbbd727a895fd14441
Jonathan Chan <jonchan816@gmail.com>
2018-11-14 17:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function dwarfRollCall(dwarves) {
+  function rollCall(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    var position = i + 1;
+    newArray.push(position, array[i])
+  }
+  return newArray.join('. ');
+}
 }
 
 function summonCaptainPlanet(planeteerCalls){
