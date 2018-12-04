b95ddaa14eae019fd18be3ccea4fa84fca27c2de
Jonathan Chan <jonchan816@gmail.com>
2018-12-04 22:22:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function dwarfRollCall(dwarves) {
+
   var text = "";
-  for (var i = 0; i < dwarves.length; i++) {
+  for (var i = 0; i < dwarves.length/2; i++) {
     var position = i + 1;
     text += `${position}. ${dwarves[i]} `;
   }
   return text;
-}
+} 
+
 
 function summonCaptainPlanet(planeteerCalls){
   var newArray = [];
@@ -16,18 +18,29 @@ function summonCaptainPlanet(planeteerCalls){
 }
 
 function longPlaneteerCalls(words) {
-  if (words.length < 4) {
-    return false;
-  } else {
-    return true;
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4) {
+      return true;
+    } 
   }
+  return false;
 }
 
 function findTheCheese (foods) {
   for (var i = 0; i < foods.length; i++){
-    if ((`${foods[i]}` === "cheddar") || (`${foods[i]}` === "gouda") || (`${foods[i]}` === "camembert")) {
+    if ((`${foods[i]}` === "cheddar") || (`${foods[i]}` === "gouda") || (`${foods[i]}` === "camembert") || (`${foods[i]}` === "swiss"))  {
       return `${foods[i]}`; 
     }
   }
   return "no cheese!";
+}
+
+function wordsWithB (words) {
+  new_words = [];
+  for (var i = 0; i < words.length; i++) {
+    if (words[i][0] === "b" || "B") {
+      new_words.push(words[i]);
+    }
+  }
+return new_words;
 }
